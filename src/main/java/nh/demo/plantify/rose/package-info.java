nilshartmann-workshops/spring-modulith-tk-dev-talk

04c: Explizite Abhängigkeiten

src/main/java/nh/demo/plantify/rose/package-info.java
@@ -1,0 +1,7 @@
+@ApplicationModule(
+    displayName = "Individual Care Tasks for Roses 🌹",
+    allowedDependencies = {"care :: Suggestion Factory", "shared"}
+)
+package nh.demo.plantify.rose;
+
+import org.springframework.modulith.ApplicationModule;

nilshartmann/jug-darmstadt-spring-modulith

src/main/java/nh/demo/plantify/rose/package-info.java
@@ -1,0 +1,7 @@
+@ApplicationModule(
+    displayName = "Individual Care Tasks for Roses 🌹",
+    allowedDependencies = {"care :: Suggestion Factory", "plant"}
+)
+package nh.demo.plantify.rose;
+
+import org.springframework.modulith.ApplicationModule;
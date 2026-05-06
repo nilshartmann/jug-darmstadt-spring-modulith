dd586ecbf337a8170caecdcef445a34cf405e919
Nils Hartmann <nils@nilshartmann.net>
2026-05-06 20:12:21 +0200

    04c: Explizite Abhängigkeiten

--- src/main/java/nh/demo/plantify/rose/RoseCareSuggestionFactory.java
+++ src/main/java/nh/demo/plantify/rose/RoseCareSuggestionFactory.java
@@ -1,5 +1,6 @@
 package nh.demo.plantify.rose;
 
+import nh.demo.plantify.care.CareService;
 import nh.demo.plantify.care.suggestions.CareSuggestion;
 import nh.demo.plantify.care.suggestions.CareSuggestionFactory;
 import nh.demo.plantify.plant.PlantType;
@@ -10,7 +11,16 @@ import java.util.List;
 // Package private, keine öffentliche Komponente
 @Component
 class RoseCareSuggestionFactory implements CareSuggestionFactory {
+//                                  ^--- IntelliJ Bug, wird weiterhin als Fehler angezeigt, Test ist grün
 //                                                    ^--- Problem: internes API
+
+    private CareService careService;
+//                      ^--- Korrekt nicht verlaubt: CareService ist zwar public,
+//                           aber nicht Bestandteil des NamedInterfaces
+//                           das wir als einzige erlaubte Abhängigkeit
+//                           eingetragen haben --> Test!
+
+
     @Override
     public List<CareSuggestion> createSuggestion(PlantType plantType, String location) {
 //                      ^--- IntelliJ Bug, wird weiterhin als Fehler angezeigt, Test ist grün
---
+++ src/main/java/nh/demo/plantify/rose/package-info.java
@@ -0,0 +1,7 @@
+@ApplicationModule(
+    displayName = "Individual Care Tasks for Roses 🌹",
+    allowedDependencies = {"care :: Suggestion Factory", "plant"}
+)
+package nh.demo.plantify.rose;
+
+import org.springframework.modulith.ApplicationModule;
