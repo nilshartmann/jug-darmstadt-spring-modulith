3cf403b836eb45c4881894df89029fd1189cb48a
Nils Hartmann <nils@nilshartmann.net>
2026-08-20 11:27:24 +0200

    11b: Test mit direkten Abhängigkeiten ApplicationModuleTest.BootstrapMode.DIRECT_DEPENDENCIES

--- src/test/java/nh/demo/plantify/billing/BillingModuleTest.java
+++ src/test/java/nh/demo/plantify/billing/BillingModuleTest.java
@@ -1,26 +1,32 @@
 package nh.demo.plantify.billing;
 
 import nh.demo.plantify.TestcontainersConfiguration;
+import nh.demo.plantify.care.CareService;
 import nh.demo.plantify.owner.OwnerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.modulith.test.ApplicationModuleTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.assertj.core.api.Assertions.*;
 
-@ApplicationModuleTest
+@ApplicationModuleTest(ApplicationModuleTest.BootstrapMode.DIRECT_DEPENDENCIES)
 @Import(TestcontainersConfiguration.class)
 class BillingModuleTest {
+
+    @Autowired
+    ApplicationContext context;
 
     @Test
     void add_usage_record_with_setup_fee_for_registered_plant() {
 
-        // 🔎 Zeigen
-        // Startet NUR Billing-Module
-        //  das "used" aber owner
-        // -> keine owner-Services hier
-        // -> Fehler
+        // 🔎 Welche Module sind jetzt hochgefahren?
+        // care ist keine direkte Abhängigkeit -> nicht da
+        assertThat(context.getBeansOfType(CareService.class)).isEmpty();
+
+        // owner IST direkte Abhängigkeit -> da
+        assertThat(context.getBeansOfType(OwnerRepository.class)).isNotEmpty();
     }
 
 }
