bbdde6e74a52357035de3d8160165b5b4ef88ed6
Nils Hartmann <nils@nilshartmann.net>
2026-08-20 11:27:24 +0200

    05e: TX failt - asynchrone EventListener werden nicht zurückgerollt

--- src/main/java/nh/demo/plantify/plant/PlantService.java
+++ src/main/java/nh/demo/plantify/plant/PlantService.java
@@ -39,6 +39,16 @@ class PlantService {
             plant.getLocation()
         ));
 
+        if (true) {
+            // Der Fehler geht an den Client zurück ✅
+            // Aber:
+            //  UsageTracker und CareService haben (asynchron) schon committed 😢
+            // Logs:
+            //  mit Glück sieht man erst die Exception,
+            //  dann die erfolgreiche Event-Verarbeitung
+            throw new IllegalStateException("Schade...");
+        }
+
         // Care-Tasks anlegen -> jetzt im Listener (CareService.onPlantRegistered)
 //        careService.setupInitialCareTasks(
 //            plant.getId(),
